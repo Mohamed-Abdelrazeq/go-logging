994403b89e77f66b7a45225753950d62b2b22504
Mohamed-Abdelrazek-Mohamed <mabdelrazek@hindam.sa>
2024-11-11 02:57:27 +0200

    update migrations to use go-migration-helper

--- migrations/0001_initial.sql
+++ migrations/0001_initial.sql
@@ -1,8 +1,10 @@
--- POSTGRES
-DROP TABLE IF EXISTS log_records;
+-- +migrate Up
 CREATE TABLE log_records (
     id INTEGER PRIMARY KEY,
     timestamp TIMESTAMP NOT NULL,
     level TEXT NOT NULL,
     message TEXT NOT NULL
 );
+-- -migrate Down
+DROP TABLE log_records;
+		
